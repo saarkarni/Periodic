d20165f81b151da9f3a08cc464cfa97f0f5b669e
saarkarni <saar.karni@gmail.com>
2018-04-15 15:46:07 +0300

    Updated

--- periodic.py
+++ periodic.py
@@ -20,6 +20,26 @@ def display_title_bar():
 def display_periodic_table():
     print(periodic_table)
 
+
+def add_all_elements():
+    filename = "elements.txt"
+    file = open(filename,"r")
+    for line in file:
+        add_element_from_file(line)
+
+
+def add_element_from_file(element):
+    new_properties = {}
+    element_as_list = element.split()
+
+    new_properties["01 - Element Name"] = element_as_list[1]
+    new_properties["02 - Atomic Number"] = element_as_list[2]
+    new_properties["03 - Row"] = element_as_list[3]
+    new_properties["04 - Column"] = element_as_list[4]
+
+    periodic_table[element_as_list[0]] = new_properties
+
+
 def add_element():
     # Add a new element to the data base
 
@@ -33,17 +53,32 @@ def add_element():
     else:
         # Get the properties of the new element
         new_properties = {}
-        new_properties["name"] = input("\nPlease enter the name of the element\n")
-        new_properties["atomic_number"] = input("\nPlease enter the atomic number of the element\n")
-        new_properties["row"] = input("\nPlease enter the row of the element\n")
-        new_properties["column"] = input("\nPlease enter the column of the element\n")
+        new_properties["01 - Element Name"] = input("\nPlease enter the name of the element\n")
+        new_properties["02 - Atomic Number"] = input("\nPlease enter the atomic number of the element\n")
+        new_properties["03 - Row"] = input("\nPlease enter the row of the element\n")
+        new_properties["04 - Column"] = input("\nPlease enter the column of the element\n")
 
         # Add a new element to the data base
         periodic_table[symbol] = new_properties
 
 
+def display_element_information(symbol):
+    element_details = periodic_table[symbol]
+    for characteristic, value in sorted(element_details.items()):
+        print("\n%s:" % characteristic)
+        print("%s" % value)
+
+
+def print_property_for_elements(key):
+    for element, properties in periodic_table.items():
+            print("\n" + element + ":")
+            print(properties[key])
+
+
 ### MAIN PROGRAM ###
 periodic_table = {}
+add_all_elements()
+
 user_choice = ""
 
 while user_choice != "Q":
@@ -58,10 +93,41 @@ while user_choice != "Q":
 
     elif user_choice == "I":
         # See the information about a specific element
-        element_symbol = input("\nEnter the symbol of the element on which you want the information")
+        element_symbol = input("\nEnter the symbol of the element on which you want the information: ")
 
-    #elif user_choice == "P":
+        if element_symbol not in periodic_table.keys():
+            print("Sorry, information is not available")
+
+        else:
+            display_element_information(element_symbol)
+
+    elif user_choice == "P":
         # See the property for each element in the table
+        print("Available properties are: ")
+
+        # Print the list of available properties - but only once
+        for properties in periodic_table.values():
+            for property_name in sorted(properties.keys()):
+                print(property_name)
+            break
+
+        elements_property = input("\nEnter the number of the property which you want to see: ")
+
+        if elements_property == "01":
+            print_property_for_elements("01 - Element Name")
+
+        elif elements_property == "02":
+            print_property_for_elements("02 - Atomic Number")
+
+        elif elements_property == "03":
+            print_property_for_elements("03 - Row")
+
+        elif elements_property == "04":
+            print_property_for_elements("04 - Col")
+
+        else:
+            print("Sorry, no such property...")
+
 
     elif user_choice == "T":
         # See the entire table
